63b5f7be587ca8c08c0b59d7322075e266d50649
Christian Meisel <christian@meisel.de>
2024-11-22 16:11:12 +0100

    Updates

---
+++ precache-manifest.08da79d60ac9b586af1d7991a2cf6dd8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2e5ef1e6845a8483d46ae8b0a72d3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e1f31048864b1c164a",
+    "url": "/static/css/2.0f08eb85.chunk.css"
+  },
+  {
+    "revision": "ee3d725b968ae10bc05a",
+    "url": "/static/css/main.3509f70e.chunk.css"
+  },
+  {
+    "revision": "79e1f31048864b1c164a",
+    "url": "/static/js/2.f3e2d4a5.chunk.js"
+  },
+  {
+    "revision": "8bf7614a6420c3456690",
+    "url": "/static/js/3.2d98378e.chunk.js"
+  },
+  {
+    "revision": "ee3d725b968ae10bc05a",
+    "url": "/static/js/main.c865830e.chunk.js"
+  },
+  {
+    "revision": "11daa84bf1169ffba932",
+    "url": "/static/js/runtime~main.a8504550.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
